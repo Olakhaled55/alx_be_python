801d28adb82cb863bd3722ea5a3c0a492ce9395e
Olakhaled55 <olakhaled611@gmail.com>
2024-12-21 20:29:59 +0200

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,5 @@
 number=int(input("Enter a number to see its multiplication table:")
 number=("1,10")
 for number in range(1,11)
-print(multiplication number for {number}:") (X * Y = Z) # X is the user’s number, Y is the current number in the loop, and Z is the product
+def print_equation(x,y,z):
+  print(f"{x}*{y}={z}")  # X is the user’s number, Y is the current number in the loop, and Z is the product
